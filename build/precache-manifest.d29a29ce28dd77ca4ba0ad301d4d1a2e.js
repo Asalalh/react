e038b9bd05be6c9ff2a301840be3e41c6b2cd24c
Asalalh <105460833+Asalalh@users.noreply.github.com>
2022-05-18 12:10:28 +0200

    Add files via upload

---
+++ build/precache-manifest.d29a29ce28dd77ca4ba0ad301d4d1a2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525d78484b3d64484340876532708408",
+    "url": "/Asalalh/react_project_final0/index.html"
+  },
+  {
+    "revision": "22057dffe600e0b6d694",
+    "url": "/Asalalh/react_project_final0/static/css/main.b9cd4a2c.chunk.css"
+  },
+  {
+    "revision": "242a82493a225fb030d3",
+    "url": "/Asalalh/react_project_final0/static/js/2.f8f05e5c.chunk.js"
+  },
+  {
+    "revision": "22057dffe600e0b6d694",
+    "url": "/Asalalh/react_project_final0/static/js/main.19b45714.chunk.js"
+  },
+  {
+    "revision": "2c87b5b68171623d1359",
+    "url": "/Asalalh/react_project_final0/static/js/runtime~main.8805b3dc.js"
+  }
+]);
